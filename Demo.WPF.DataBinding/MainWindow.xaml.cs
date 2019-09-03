3b298d6515d8de9de0c17fa095fc6075956ae988
mohamadalitellawi <mohamadalitellawi@gmail.com>
2019-09-03 17:56:09 +0300

    First Run

--- Demo.WPF.DataBinding/MainWindow.xaml.cs
+++ Demo.WPF.DataBinding/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace Demo.WPF.DataBinding
         {
             InitializeComponent();
         }
+
+
+
+        public string AuthorName
+        {
+            get { return (string)GetValue(AuthorNameProperty); }
+            set { SetValue(AuthorNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for AuthorName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AuthorNameProperty =
+            DependencyProperty.Register("AuthorName", typeof(string), typeof(MainWindow), new PropertyMetadata("Mohamad Ali"));
+
+
+
+        public string BookName
+        {
+            get { return (string)GetValue(BookNameProperty); }
+            set { SetValue(BookNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BookName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BookNameProperty =
+            DependencyProperty.Register("BookName", typeof(string), typeof(MainWindow), new PropertyMetadata("Visual Studio"));
+
+
+
+        public string PublishDate
+        {
+            get { return (string)GetValue(PublishDateProperty); }
+            set { SetValue(PublishDateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PublishDate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PublishDateProperty =
+            DependencyProperty.Register("PublishDate", typeof(string), typeof(MainWindow), new PropertyMetadata("2019"));
+
+
+
+
     }
 }
